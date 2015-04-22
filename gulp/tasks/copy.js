06b9848006e59118fe0ea553d92a1c4ad1688424
huang.xinghui <huang.x.hui@gmail.com>
2015-04-22 14:34:45 +0800

    add annotation

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -4,6 +4,7 @@
 var gulp = require('gulp');
 var vendor = require('../config').vendor;
 
+// copy bootstrap to vendor.src directory
 gulp.task('copy', function () {
     gulp.src([vendor.src + '/bootstrap/dist/**/*'])
         .pipe(gulp.dest(vendor.dest + '/bootstrap'));
